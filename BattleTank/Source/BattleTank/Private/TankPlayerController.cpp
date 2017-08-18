57165bc914201d1704134256915f0a94d54d6a01
loshjawrence <loshjawrence@gmail.com>
2017-08-18 18:26:09 -0400

    first steps to aiming, finished lec127

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -14,9 +14,29 @@ void ATankPlayerController::BeginPlay() {
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair() {
+	if (!GetControlledTank()) { return; }
 
+	FVector HitLocation; //out param
+	if (GetSightRayHitLocation(OUT HitLocation)) { //has 'side-effect', is going to ray trace
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		//tell tank controlled tank to aim at this point
+	}
+}
+
+//Get world loc if raytrace through crosshair, true if hits land
+bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const {
+
+	OutHitLocation = FVector(1.f);
+	return true;
+}
